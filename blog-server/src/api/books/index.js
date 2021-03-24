603fb586a2d4f681b15784fb3d88d408d9c2ab72
develjsh <develJsh@gmail.com>
2021-03-25 00:27:38 +0900

    21-03-25
    모델 메소드 만들기

--- blog-server/src/api/books/index.js
+++ blog-server/src/api/books/index.js
@@ -1,12 +1,13 @@
-const Router = require('koa-router');
+const Router = require("koa-router");
 
 const books = new Router();
-const booksCtrl = require('./books.controller');
+const booksCtrl = require("./books.controller");
 
-books.get('/', booksCtrl.list);
-books.post('/', booksCtrl.create);
-books.delete('/', booksCtrl.delete);
-books.put('/', booksCtrl.replace);
-books.patch('/', booksCtrl.update);
+books.get("/", booksCtrl.list);
+books.get("/:id", booksCtrl.get);
+books.post("/", booksCtrl.create);
+books.delete("/", booksCtrl.delete);
+books.put("/", booksCtrl.replace);
+books.patch("/", booksCtrl.update);
 
 module.exports = books;
--- blog-server/src/index.js
+++ blog-server/src/index.js
@@ -1,36 +1,33 @@
-require('dotenv').config(); // .env 파일에서 환경변수 불러오기
+require("dotenv").config(); // .env 파일에서 환경변수 불러오기
 
-const Koa = require('koa');
-const Router = require('koa-router');
+const Koa = require("koa");
+const Router = require("koa-router");
 
 const app = new Koa();
 const router = new Router();
-const api = require('./api');
+const api = require("./api");
 
-const mongoose = require('mongoose');
-const bodyParser = require('koa-bodyparser');
+const mongoose = require("mongoose");
+const bodyParser = require("koa-bodyparser");
 
 const { PORT, MONGO_URI } = process.env;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(e => {
-        console.error(e);
-    });
-
-router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
-
-app.use(router.routes()).use(router.allowedMethods());
-
-const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 app.use(bodyParser()); // 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
+router.use("/api", api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 
-router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 app.use(router.routes()).use(router.allowedMethods());
 
+const port = PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+
 app.listen(port, () => {
-    console.log('heurm server is listening to port ' + port);
+  console.log("heurm server is listening to port " + port);
 });
