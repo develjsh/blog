5fa35b485c733f5bdf253696ab1691d0fe674e1e
develjsh <develJsh@gmail.com>
2021-03-25 17:41:56 +0900

    2021-03-25 backend done

--- blog-server/src/index.js
+++ blog-server/src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config(); // .env 파일에서 환경변수 불러오기
-
+const { jwtMiddleware } = require("lib/token");
 const Koa = require("koa");
 const Router = require("koa-router");
 
@@ -22,8 +22,8 @@ mongoose
   });
 
 app.use(bodyParser()); // 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
+app.use(jwtMiddleware);
 router.use("/api", api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
-
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
