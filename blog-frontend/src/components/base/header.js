66e897cf881d1fb0e4ddd70bdfa6bd20658d70b7
develjsh <develJsh@gmail.com>
2021-03-26 13:43:48 +0900

    2021-03-26 13:43 회원가입과 로그인 구현 [1] DONE

---
+++ blog-frontend/src/components/base/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const headerBlock = styled.div``;
+
+const header = () => {
+  return <headerBlock></headerBlock>;
+};
+
+export default header;
